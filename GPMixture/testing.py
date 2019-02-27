3efe524ef0ae090c241c2d6af4f021ca8b7f7f6a
karenlc <karen.lcarbajal@gmail.com>
2019-02-27 01:40:20 -0600

    Reorganizacion del codigo. Agregamos funcion que compara el error de la prediccion del goal y subgoals.

--- GPMixture/testing.py
+++ GPMixture/testing.py
@@ -8,6 +8,8 @@ Testing functions
 from GPRlib import *
 from path import *
 from plotting import *
+from kernels import *
+import matplotlib.pyplot as plt
 import numpy as np
 import math
 import GPRlib
@@ -44,15 +46,12 @@ def trajectory_prediction_test(x,y,l,knownN,startG,finishG,goals,unitMat,stepUni
     
     newL, finalL = get_prediction_set(lastKnownPoint,finalPoints[0],unit,stepUnit)
     finalArcLen = []
-    
     for i in range(1):#len(finalPoints)):
         finalArcLen.append(get_arclen_to_finish_point(lastKnownPoint,finalPoints[i],unit))
         trueX.append(finalPoints[i][0])    
         trueY.append(finalPoints[i][1])
         trueL.append(finalArcLen[i])
         
-    #print("[final points]:",finalPoints)
-    #print("[final arclen]:",finalArcLen)
     newX,newY,varX,varY = prediction_XY(trueX,trueY,trueL,newL,kernelX,kernelY) 
     return newX, newY, varX, varY
     
@@ -66,14 +65,12 @@ def subgoal_prediction(x,y,l,knownN,subgoal,unit,stepUnit,kernelX,kernelY):
     newL, finalL = get_prediction_set(lastKnownPoint,finalPoints[0],unit,stepUnit)
     finalArcLen = []
     
-    for i in range(len(finalPoints)):
+    for i in range(1):#len(finalPoints)):
         finalArcLen.append(get_arclen_to_finish_point(lastKnownPoint,finalPoints[i],unit))
         trueX.append(finalPoints[i][0])    
         trueY.append(finalPoints[i][1])
         trueL.append(finalArcLen[i])
         
-    #print("[final points]:",finalPoints)
-    #print("[final arclen]:",finalArcLen)
     newX,newY,varX,varY = prediction_XY(trueX,trueY,trueL,newL,kernelX,kernelY) 
     return newX, newY, varX, varY
 
@@ -85,7 +82,7 @@ def trajectory_subgoal_prediction_test(img,x,y,l,knownN,startG,finishG,goals,uni
     trueX, trueY, trueL = get_known_set(x,y,l,knownN)
     lastKnownPoint = [x[knownN-1], y[knownN-1], l[knownN-1] ]
     unit = unitMat[startG][finishG]
-    #goalCenter = middle_of_area(goals[finishG])
+    
     nSubgoals = 2
     subgoalsXY, size = get_subgoals_center_and_size(nSubgoals,goals[finishG],samplingAxis[finishG])
     predictedXYVec, varXYVec = [], []    
@@ -97,38 +94,61 @@ def trajectory_subgoal_prediction_test(img,x,y,l,knownN,startG,finishG,goals,uni
         predictedXYVec.append([predX,predY])
         varXYVec.append([varX,varY])
     
-    """
-    sampleX, sampleY, axis = uniform_sampling_1D(nSubgoals,goals[finishG],samplingAxis[finishG])
-    subgoalsXY = []
-    for i in range(nSubgoals):
-        subgoalsXY.append([sampleX[i], sampleY[i]])
-    
-    predictedXYVec, varXYVec = [],[]
-    for i in range(nSubgoals):
-        newL, finalL = get_prediction_set(lastKnownPoint,subgoalsXY[i],unit,stepUnit)
-        trueX.append(subgoalsXY[i][0])    
-        trueY.append(subgoalsXY[i][1])
-        trueL.append(finalL)
-        newX,newY,varX,varY = prediction_XY(trueX,trueY,trueL,newL,kernelX,kernelY) 
-        predictedXYVec.append([newX,newY])
-        varXYVec.append([varX,varY])
-        trueX.pop()
-        trueY.pop()
-        trueL.pop()
-        
-    subgoalElipseX, subgoalElipseY = 0,0
-    if samplingAxis[finishG] == 'x':
-        subgoalElipseX = (goals[finishG][len(goals[finishG]) -2] - goals[finishG][0])/nSubgoals 
-        subgoalElipseY = (goals[finishG][len(goals[finishG]) -1] - goals[finishG][1])/2
-    
-    if samplingAxis[finishG] == 'y':
-        subgoalElipseX = (goals[finishG][len(goals[finishG]) -2] - goals[finishG][0])/2 
-        subgoalElipseY = (goals[finishG][len(goals[finishG]) -1] - goals[finishG][1])/nSubgoals
-    """
-    
-    elipse = size#[subgoalElipseX,subgoalElipseY]
-    print("[Elipse]:", elipse)
+    elipse = size
     plot_subgoal_prediction(img,trueX,trueY,knownN,nSubgoals,predictedXYVec,varXYVec,elipse)
+
+def subgoal_prediction_test(nSubgoals,x,y,l,knownN,startG,finishG,goals,unitMat,stepUnit,kernelMatX,kernelMatY,samplingAxis):
+    kernelX = kernelMatX[startG][finishG]
+    kernelY = kernelMatY[startG][finishG]
+    
+    trueX, trueY, trueL = get_known_set(x,y,l,knownN)
+    lastKnownPoint = [x[knownN-1], y[knownN-1], l[knownN-1] ]
+    unit = unitMat[startG][finishG]
+    
+    subgoalsXY, size = get_subgoals_center_and_size(nSubgoals,goals[finishG],samplingAxis[finishG])
+    predictedXYVec, varXYVec = [], []    
+    for i in range(nSubgoals):
+        _x, _y = subgoalsXY[i][0], subgoalsXY[i][1]
+        lx, ly = size[0]/2, size[1]/2
+        subgoal = [_x -lx, _y -ly, _x +lx, _y -ly,_x -lx, _y +ly, _x +lx, _y +ly]
+        predX, predY, varX, varY = subgoal_prediction(x,y,l,knownN,subgoal,unit,stepUnit,kernelX,kernelY)
+        predictedXYVec.append([predX,predY])
+        varXYVec.append([varX,varY])
+    
+    return predictedXYVec, varXYVec
+
+def compare_error_goal_to_subgoal_test(img,x,y,l,startG,finishG,goals,unitMat,stepUnit,kernelMatX,kernelMatY,samplingAxis):
+    nSubgoals = 2  
+    goalError = []
+    subgoalError = []
+    index = []
+    part_num = 5
+    traj_len = len(x)
+    for i in range(1,part_num-1):
+        index.append(i)
+        knownN = int((i+1)*(traj_len/part_num))    
+    
+        trueX, trueY = [], []
+        for j in range(knownN,traj_len):
+            trueX.append(x[j])
+            trueY.append(y[j])
+    
+        goalPredX, goalPredY, goalVarX, goalVarY = trajectory_prediction_test(x,y,l,knownN,startG,finishG,goals,unitMat,stepUnit,kernelMatX,kernelMatY)
+        subPredXYVec, subVarXYVec = subgoal_prediction_test(nSubgoals,x,y,l,knownN,startG,finishG,goals,unitMat,stepUnit,kernelMatX,kernelMatY,samplingAxis)
+        plot_prediction(img,x,y,knownN,goalPredX, goalPredY, goalVarX, goalVarY,[0,0])
+        plot_subgoal_prediction(img,x,y,knownN,nSubgoals,subPredXYVec,subVarXYVec,[0,0])        
+        
+        gError = average_displacement_error([trueX,trueY],[goalPredX,goalPredY])
+        sgErrorVec = []
+        for k in range(nSubgoals):
+            sgErrorVec.append(average_displacement_error([trueX,trueY], subPredXYVec[k]))
+        goalError.append(gError)
+        subgoalError.append( min(sgErrorVec) )
+    
+    print("[goal Error]",goalError)
+    print("[subgoal Error]",subgoalError)
+    plt.plot(index,goalError,'m')
+    plt.plot(index,subgoalError,'b')
 
 #recibe: datos conocidos, valores por predecir, areas de inicio y final
 def prediction_test_over_time(x,y,z,knownN,start,end,goals):
@@ -146,7 +166,7 @@ def prediction_test_over_time(x,y,z,knownN,start,end,goals):
     plot_prediction(img,x,y,knownN,newX,newY,varX,varY)
 
 #recibe: datos conocidos, valores por predecir, areas de inicio y final
-def prediction_test(x,y,z,knownN,startG,finishG,goals,unitMat,meanLenMat,steps):
+def prediction_test(img,x,y,z,knownN,startG,finishG,goals,unitMat,meanLenMat,steps,kernelMat_x,kernelMat_y):
     kernelX = kernelMat_x[startG][finishG]
     kernelY = kernelMat_y[startG][finishG]
     
@@ -155,15 +175,16 @@ def prediction_test(x,y,z,knownN,startG,finishG,goals,unitMat,meanLenMat,steps):
     unit = unitMat[startG][finishG]
     meanLen = meanLenMat[startG][finishG]
     
-    final_xy = get_finish_point(trueX,trueY,trueZ,finishG,goals,kernelX,kernelY,unit,goalSamplingAxis)
+    #final_xy = get_finish_point(trueX,trueY,trueZ,finishG,goals,kernelX,kernelY,unit,goalSamplingAxis)
     #final_xy = get_finish_point_singleGP(trueX,trueY,trueZ,finishG,goals,kernelX,kernelY,unit,img)
-    newZ, final_z = get_prediction_set(lastKnownPoint,final_xy,unit,steps)  
+    final_xy = middle_of_area(goals[finishG])
+    newZ, final_z = get_prediction_set_given_size(lastKnownPoint,final_xy,unit,steps) 
     trueX.append(final_xy[0])    
     trueY.append(final_xy[1])
     trueZ.append(final_z)
     
     newX,newY,varX,varY = prediction_XY(trueX,trueY,trueZ,newZ,kernelX,kernelY) 
-    plot_prediction(img,x,y,knownN,newX,newY,varX,varY)
+    plot_prediction(img,x,y,knownN,newX,newY,varX,varY,[0,0])
     
 #recibe: datos reales, num de datos conocidos, area de inicio, vec de areas objetivo, vec de areas 
 def _multigoal_prediction_test(x,y,z,knownN,startG,finishG,goals,unitMat,steps,kernelMat_x,kernelMat_y):
