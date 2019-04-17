c667bf875df5c5bbde3c8b1faa90b1a98c752dc0
karenlc <karen.lcarbajal@gmail.com>
2019-04-16 21:24:34 -0500

    Reacomodo del codigo. Dejamos solo las funciones de la clase path.

--- GPMixture/path.py
+++ GPMixture/path.py
@@ -13,11 +13,30 @@ class path:
         self.t = vecT
         self.x = vecX
         self.y = vecY
-        self.l = pathArcLength(self.x,self.y)
+        self.l = path_arcLength(self.x,self.y)
         self.duration, self.length = statistics(self.t,self.x,self.y)
         if self.duration > 0:            
             self.v = self.length/self.duration
         else:
             self.v = -1.
 
+def statistics(t,x,y):
+    duration = t[len(t)-1] - t[0]
+        
+    l = path_arcLength(x,y)
+    length = l[len(l)-1]
+    
+    return duration, length
+
+def path_arcLength(x,y):
+    n = len(x)
+    l = [0]
+    for i in range(n):
+        if i > 0:
+            l.append(np.sqrt( (x[i]-x[i-1])**2 + (y[i]-y[i-1])**2 ) )
+    for i in range(n):
+        if(i>0):
+            l[i] = l[i] +l[i-1]
+    return l    
+    
 #************************************************************#
