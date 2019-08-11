d3a64e6c45f2aae28b4876bfaec96f97f55227cf
karenlc <karen.lcarbajal@gmail.com>
2019-08-10 20:48:21 -0500

    In prediction_to_finish_point() we check if the var matrix is positive definite and if we can't call cholesky() then we make var its nearest positive definite.

--- GPMixture/gp_code/evaluation.py
+++ GPMixture/gp_code/evaluation.py
@@ -3,6 +3,7 @@ Error and likelihood evaluation
 """
 import numpy as np
 import math
+from numpy import linalg as la
 from gp_code.regression import *
 from utils.dataManagement import *
 
@@ -145,3 +146,31 @@ def ADE_of_prediction_given_future_steps(fullPath, predictedXY, knownN, futureSt
 
     error = mean_displacement_error([realX,realY],[predX,predY])
     return error
+    
+def nearestPD(A):
+    B = (A + np.transpose(A)) / 2
+    _, s, V = la.svd(B)
+    H = np.dot(np.transpose(V), np.dot(np.diag(s), V))
+    A2 = (B + H) / 2
+    A3 = (A2 + np.transpose(A2)) / 2
+
+    if isPD(A3):
+        return A3
+
+    spacing = np.spacing(la.norm(A))
+    I = np.eye(A.shape[0])
+    k = 1
+    while not isPD(A3):
+        mineig = np.min(np.real(la.eigvals(A3)))
+        A3 += I * (-mineig * k**2 + spacing)
+        k += 1
+
+    return A3
+
+def isPD(B):
+    """Returns true when input is positive-definite, via Cholesky"""
+    try:
+        _ = la.cholesky(B)
+        return True
+    except la.LinAlgError:
+        return False
