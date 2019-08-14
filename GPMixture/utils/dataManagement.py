50425d2d723b7e5bdf8628150f98daf3f3b34b6b
karenlc <karen.lcarbajal@gmail.com>
2019-08-13 21:39:07 -0500

    Added functions to read and write data on files.

--- GPMixture/utils/dataManagement.py
+++ GPMixture/utils/dataManagement.py
@@ -139,6 +139,26 @@ def write_useful_paths_file(paths): #paths es un vector de indices
             f.write(s)
     f.close()
 
+
+def write_data(data, fileName):
+    n = len(data)
+    f = open(fileName,"w+")
+    for i in range(n):
+        s = "%d\n"%(data[i])
+        f.write(s)
+    f.close()
+    
+def read_data(fileName):
+    data = []
+    f = open(fileName,'r')
+    lines = f.readlines()
+    for i in range(len(lines)):
+        lines[i] = lines[i].strip("\n")
+        data.append( float(lines[i]) )
+    f.close()
+    return data
+    
+
 """ FILTER PATHS """
 """ Regresa una matriz de trayectorias:
 en la entrada (i,j) estan los caminos que comienzan en g_i y terminan en g_j"""
