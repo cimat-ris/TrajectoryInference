5eaffcba9853a45a1c06dd0e8db5ef049dc4e194
JB Hayet <jbhayet@cimat.mx>
2020-04-22 22:43:27 -0500

    Added checks in the parametes reading function

--- GPMixture/utils/io_parameters.py
+++ GPMixture/utils/io_parameters.py
@@ -1,26 +1,36 @@
 import numpy as np
+import sys
 from gp_code.kernels import create_kernel_matrix
 
 # Takes as an input a matrix of kernels. Exports the parameters, line by line
-def write_parameters(matrix,rows,columns,fileName):
+def write_parameters(matrix,rows,columns,fileName,kernelType='linePriorCombined'):
     f = open(fileName,"w")
-    f.write('%d %d %s\n' % (rows,columns,matrix[0][0].type))
+    f.write('%d %d %s\n' % (rows,columns,kernelType))
     for i in range(rows):
         for j in range(columns):
             ker = matrix[i][j]
-            f.write('{:d} {:d} '.format(i,j))
-            parameters = ker.get_parameters()
-            for k in range(len(parameters)):
-                f.write('{:07.4f} '.format(parameters[k]))
-            skip = "\n"
-            f.write(skip)
+            if ker!=None:
+                f.write('{:d} {:d} '.format(i,j))
+                parameters = ker.get_parameters()
+                for k in range(len(parameters)):
+                    f.write('{:07.4f} '.format(parameters[k]))
+                skip = "\n"
+                f.write(skip)
     f.close()
 
 # Read a parameter file and return the matrix of kernels corresponding to this file
 def read_and_set_parameters(file_name, nParameters):
-    file = open(file_name,'r')
+    try:
+        file = open(file_name,'r')
+    except OSError:
+        print("[ERR] Could not open/read file: ",file_name)
+        sys.exit()
+
     firstline = file.readline()
     header    = firstline.split()
+    if len(header)<3:
+        print("[ERR] Problem with the header of file: ",file_name)
+        sys.exit()
     # Get rows, columns, kernelType from the header
     rows      = int(header[0])
     columns   = int(header[1])
