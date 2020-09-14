ae46ddacc9b486b7a5923430007eaea5a8d1ca49
JBH <jbhayet@cimat.mx>
2020-09-13 19:16:29 -0500

    Update __init__.py, test_animation.py, and 18 more files...

--- GPMixture/examples/test_train.py
+++ GPMixture/examples/test_train.py
@@ -1,16 +1,17 @@
 """
 @author: karenlc
 """
-from gp_code.goal_pairs import *
-from utils.plotting import plotter
-from utils.io_parameters import *
+from test_common import *
+#from gp_code.goal_pairs import *
+#from utils.plotting import plotter
+#from utils.io_parameters import *
 from utils.manip_trajectories import get_paths_in_areas,time_compare,filter_path_matrix,define_trajectories_start_and_end_areas
 from utils.io_trajectories import get_paths_from_file
-from utils.stats_trajectories import get_data_from_paths
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-import pandas as pd
+#from utils.stats_trajectories import get_data_from_paths
+#import numpy as np
+#import matplotlib.pyplot as plt
+#import matplotlib.image as mpimg
+#import pandas as pd
 
 # Read the areas data from a csv file
 data     = pd.read_csv('parameters/CentralStation_GoalsDescriptions.csv')
@@ -22,16 +23,15 @@ nGoals    = len(areas)
 img       = mpimg.imread('imgs/goals.jpg')
 
 # This function segments the trajectories in function of the goal areas
-dataPaths, multigoal = get_paths_from_file('datasets/CentralStation_paths_10000.txt',areas)
+dataPaths,__ = get_paths_from_file('datasets/CentralStation_paths_10000.txt',areas)
 # Filter paths
-usefulPaths          = get_paths_in_areas(dataPaths,areas)
-print("[INF] Number of useful paths: ",len(usefulPaths))
+usefulPaths  = get_paths_in_areas(dataPaths,areas)
 
 # Split the trajectories into pairs of goals
 startToGoalPath, arclenMat = define_trajectories_start_and_end_areas(areas,areas,usefulPaths)
-# Remove the trajectories that are either too short or too long
-pathMat, learnSet = filter_path_matrix(startToGoalPath, nGoals, nGoals)
-sortedPaths = sorted(learnSet, key=time_compare)
+# Remove the trajectories that are either too short or too long (within a few stddev of the median)
+pathMat, learnSet          = filter_path_matrix(startToGoalPath, nGoals, nGoals)
+sortedPaths                = sorted(learnSet, key=time_compare)
 
 # Form the object goal_pairs
 goalsData = goal_pairs(areas,areasAxis,pathMat)
@@ -42,21 +42,22 @@ if showDataset:
     s = areas.shape
     for i in range(s[0]):
         for j in range(s[0]):
-            p = plotter(img,title=f"Trajectories from {i} to {j}")
-            p.plot_scene_structure(goalsData)
-            p.plot_paths(pathMat[i][j])
-            p.show()
+            if len(pathMat[i][j])>0:
+                p = plotter(img,title=f"Trajectories from {i} to {j}")
+                p.plot_scene_structure(goalsData)
+                p.plot_paths(pathMat[i][j])
+                p.show()
 
+print("[INF] Number of original paths: ",len(dataPaths))
 print("[INF] Number of filtered paths: ",len(learnSet))
 
 # For each pair of goals, determine the line priors
 goalsData.compute_linear_priors(pathMat)
 
 # Selection of the kernel type
-kernelType = "linePriorCombined"#"combined"
-nParameters = 4
+kernelType  = "linePriorCombined"
 
-"""**************    Learning parameters     **************************"""
+"""**************    Learning GP parameters     **************************"""
 print("[INF] Starting the learning phase")
 goalsData.optimize_kernel_parameters(kernelType, pathMat)
 write_parameters(goalsData.kernelsX,nGoals,nGoals,"parameters/linearpriorcombined6x6_x.txt")
