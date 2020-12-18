f58195f35af495323f2e7115d0edfc8bd8f540f4
karenlc <aklopezcarbajal@gmail.com>
2020-12-18 13:00:18 -0500

    Update test_plotspeeds

--- GPMixture/examples/test_plotspeeds.py
+++ GPMixture/examples/test_plotspeeds.py
@@ -3,24 +3,36 @@
 """
 from test_common import *
 from gp_code.single_gp import singleGP
-from utils.stats_trajectories import get_data_from_paths
+from utils.stats_trajectories import trajectory_arclength
 import matplotlib.pyplot as plt
+import random
 
-img         = mpimg.imread('imgs/goals.jpg')
-station_img = mpimg.imread('imgs/train_station.jpg')
 # Read the areas file, dataset, and form the goalsLearnedStructure object
-goalsData, pathMat, __ = read_and_filter('parameters/CentralStation_GoalsDescriptions.csv','datasets/GCS/CentralStation_trainingSet.txt')
-stepUnit  = 0.0438780780171   #get_number_of_steps_unit(pathMat, nGoals)
+traj_dataset, goalsData, trajMat, __ = read_and_filter('GCS','../parameters/CentralStation_GoalsDescriptions.csv','../datasets/GC/Annotation/',use_pickled_data=True)
 
-# We give the start and ending goals
-startG  = 0
-nextG   = 6
-x,y,t,l,s = get_data_from_paths(pathMat[startG][nextG])
+trajSet = trajMat[0][6]
+localSpeed = []
+for tr in trajSet:
+    t = tr[2]
+    d = trajectory_arclength(tr)
+    trSpeed = [0.0]
+    for i in range(1,len(t)):
+        trSpeed.append(float( (d[i]-d[i-1])/(t[i]-t[i-1]) ) )
+    localSpeed.append(trSpeed)
+
 
 fig,ax = plt.subplots(1)
-n = min(len(s),30)
-for i in range(n):
-    plt.plot(s[i])
+
+""" #This trajectory has an abnormal value at the end
+traj = trajSet[130]
+for i in range(len(traj[0])):
+    plt.plot(traj[0][i],traj[1][i], 'b+' )
+"""
+#Plot a random sample of the trajectory set
+sample = random.choices(range(len(trajSet)), k=30)
+
+for i in range(len(sample)):
+    plt.plot(localSpeed[sample[i]])
 plt.ylabel('Speed')
-plt.xlabel('Time step')
+plt.xlabel('Time')
 plt.show()
