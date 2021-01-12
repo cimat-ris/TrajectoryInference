3845ee02223fb0a54dd0e53d01ef9954ed51d5a9
karenlc <aklopezcarbajal@gmail.com>
2021-01-12 13:04:51 -0500

    Modifying class path_regression.

--- GPMixture/gp_code/path_regression.py
+++ GPMixture/gp_code/path_regression.py
@@ -4,9 +4,10 @@ A class for GP-based path regression
 import numpy as np
 import math
 from gp_code.regression import *
-from gp_code.path1D_regression import path1D_regression
 from gp_code.sampling import *
+from gp_code.path1D_regression import path1D_regression
 from utils.manip_trajectories import goal_center_and_size
+from utils.stats_trajectories import euclidean_distance
 from scipy.optimize import bisect
 
 class path_regression:
@@ -25,64 +26,64 @@ class path_regression:
     # Update observations for the Gaussian process (matrix K)
     def updateObservations(self,observedX,observedY,observedL):
         # Last really observed point
-        lastObservedPoint = [observedX[-1], observedY[-1], observedL[-1]]
+        lastObs = [observedX[-1], observedY[-1], observedL[-1]]
         # Determine the set of arclengths (predictedL) to predict
-        self.predictedL, finalL, self.dist = get_prediction_set_arclengths(lastObservedPoint,self.finalAreaCenter,self.distUnit,self.stepUnit)
+        self.predictedL, finalL, self.dist = self.prediction_set_arclength(lastObs,self.finalAreaCenter)
         # Define the variance associated to the last point (varies with the area)
         if self.finalAreaAxis==0:
             s              = self.finalAreaSize[0]
         elif self.finalAreaAxis==1:
             s              = self.finalAreaSize[1]
 
-        maxDim = 20
-        n = len(observedX)
-        if n > maxDim:
-            print("[INF] Sampling data")
-            ind = [i for i in range(1,n)]
-            p = 0.5
-            iprob = [ pow((1-p),n-i)*p for i in ind ]
-            iprob[0] = 1- (sum(iprob) -iprob[0])
-
-            #sampleInd = np.random.choice(ind, maxDim-1, replace=False, p=iprob) #geometric
-            sampleInd = np.random.choice(ind, maxDim-1, replace=False)         #normal
-
-            observedL = np.append(observedL[0], [observedL[i] for i in np.sort(sampleInd)])
-            observedX = np.append(observedX[0], [observedX[i] for i in np.sort(sampleInd)])
-            observedY = np.append(observedY[0], [observedY[i] for i in np.sort(sampleInd)])
-
         # Update observations of each process
         self.regression_x.updateObservations(observedX,observedL,self.finalAreaCenter[0],finalL,(1.0-self.finalAreaAxis)*s*s*math.exp(-self.dist/s),self.predictedL)
         self.regression_y.updateObservations(observedY,observedL,self.finalAreaCenter[1],finalL,    (self.finalAreaAxis)*s*s*math.exp(-self.dist/s),self.predictedL)
 
+    def prediction_set_arclength(self, lastObs, finishPoint):
+        # Coordinates of the last observed point
+        x, y, l = lastObs[0], lastObs[1], lastObs[2]
+        # Euclidean distance between the last observed point and the finish point
+        euclideanDist = euclidean_distance([x,y], finishPoint)
+        # Rough estimate of the remaining arc length
+        distToGoal  = euclideanDist*self.distUnit
+        
+        numSteps      = int(distToGoal*self.stepUnit)
+        newset = np.zeros((numSteps,1))
+        if numSteps > 0:
+            step = distToGoal/float(numSteps)
+            for i in range(1,numSteps+1):
+                newset[i-1,0] = l + i*step
+        return newset, l + distToGoal, distToGoal
+    
     # Filter initial observations
     def filterObservations(self):
         filteredx = self.regression_x.filterObservations()
         filteredy = self.regression_y.filterObservations()
         return filteredx,filteredy
 
-    # For a given dataset (knownX,knownY,knownL), takes half of the data as known
+    # For a given set of observations (x,y,l), takes half of the data as known
     # and predicts the remaining half. Then, evaluate the prediction error.
     def compute_prediction_error_of_points_along_the_path(self,nPoints,observedX,observedY,observedL):
         # Known data
-        observedN = len(observedX)
-        halfN     = max(1,int(observedN/2))
+        n = len(observedX)
+        half     = max(1,int(n/2))
         # First half of the known data
-        trueX  = observedX[0:halfN]
-        trueY  = observedY[0:halfN]
-        trueL  = observedL[0:halfN]
+        trueX  = observedX[0:half]
+        trueY  = observedY[0:half]
+        trueL  = observedL[0:half]
         # Get the last point and add it to the observed data
-        finishD= euclidean_distance([trueX[len(trueX)-1],trueY[len(trueY)-1]],self.finalAreaCenter)
+        distToGoal = euclidean_distance([trueX[-1],trueY[-1]],self.finalAreaCenter)*self.distUnit
         np.append(trueX,self.finalAreaCenter[0])
         np.append(trueY,self.finalAreaCenter[1])
-        np.append(trueL,finishD*self.distUnit)
-        d = int(halfN/nPoints)
+        np.append(trueL,distToGoal)
+        d = int(half/nPoints)
         if d<1:
             return 1.0
         return 1.0
         # Prepare the ground truths and the list of l to evaluate
-        realX         = observedX[halfN:halfN+nPoints*d:d]
-        realY         = observedY[halfN:halfN+nPoints*d:d]
-        predictionSet = observedL[halfN:halfN+nPoints*d:d]
+        realX         = observedX[half:half+nPoints*d:d]
+        realY         = observedY[half:half+nPoints*d:d]
+        predictionSet = observedL[half:half+nPoints*d:d]
         # TODO! redo the prediction based on GP model
         # Get the prediction based on the GP
         # Evaluate the error
