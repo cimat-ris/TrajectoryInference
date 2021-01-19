56893e4cc816afb74282e7dc22a391fd0a24b6b1
JBH <jbhayet@cimat.mx>
2021-01-18 19:20:51 -0600

    Some experiments with speed models

--- GPMixture/examples/test_plotspeeds.py
+++ GPMixture/examples/test_plotspeeds.py
@@ -3,12 +3,54 @@
 """
 from test_common import *
 from gp_code.single_gp import singleGP
-from utils.stats_trajectories import trajectory_arclength, avg_speed
+from utils.stats_trajectories import trajectory_arclength, avg_speed, median_speed
 import matplotlib.pyplot as plt
 import random
+from sklearn.linear_model import LinearRegression, HuberRegressor
+from sklearn.linear_model import Ridge
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
 
 # Read the areas file, dataset, and form the goalsLearnedStructure object
-traj_dataset, goalsData, trajMat, __ = read_and_filter('GCS','../parameters/CentralStation_GoalsDescriptions.csv','../datasets/GC/Annotation/',use_pickled_data=True)
+traj_dataset, goalsData, trajMat, __ = read_and_filter('GCS','parameters/CentralStation_GoalsDescriptions.csv','datasets/GC/Annotation/',use_pickled_data=True)
+
+
+for i in range(goalsData.nGoals):
+    for j in range(goalsData.nGoals):
+        trajSet        = trajMat[i][j]
+        # Only if we have enough trajectories
+        if len(trajSet) < goalsData.min_traj_number:
+            continue
+        relativeSpeeds = []
+        lengths        = []
+        for tr in trajSet:
+            # Times
+            t = tr[2]
+            # Average speed
+            v = avg_speed(tr)
+            # Arc lengths
+            d = trajectory_arclength(tr)
+            for k in range(1,len(t)):
+                relativeSpeeds.append(float((d[k]-d[k-1])/(t[k]-t[k-1]))/v)
+                lengths.append(d[k])
+        lengths        = np.array(lengths).reshape(-1, 1)
+        print(lengths.shape)
+        relativeSpeeds = np.array(relativeSpeeds)
+        model = make_pipeline(PolynomialFeatures(4), Ridge())
+        model.fit(lengths, relativeSpeeds)
+        slenghts = np.sort(lengths, axis=0)
+        relativeSpeedsPredicted = model.predict(slenghts)
+        p = plotter("imgs/train_station.jpg",title=f"Trajectories from {i} to {j}")
+        p.plot_scene_structure(goalsData)
+        p.plot_trajectories(trajMat[i][j])
+        #p.show()
+        fig,ax = plt.subplots(1)
+        plt.plot(lengths,relativeSpeeds,'+')
+        plt.plot(slenghts,relativeSpeedsPredicted)
+        plt.ylabel('Relative speed')
+        plt.xlabel('Length')
+        plt.show()
+
 
 trajSet       = trajMat[0][6]
 instantSpeeds = []
@@ -23,7 +65,7 @@ for tr in trajSet:
     for i in range(1,len(t)):
         trajectorySpeeds.append(float( (d[i]-d[i-1])/(t[i]-t[i-1]) ) )
     instantSpeeds.append(trajectorySpeeds)
-    averageSpeeds.append(avg_speed(tr))
+    averageSpeeds.append(median_speed(tr))
 
 
 """ #This trajectory has an abnormal value at the end
