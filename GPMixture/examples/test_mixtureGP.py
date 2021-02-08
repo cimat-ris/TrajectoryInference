a9cf151a7b53b96efdf41c179b0dd57fa6d4c727
JBH <jbhayet@cimat.mx>
2021-02-08 11:50:52 -0600

    Adapted names

--- GPMixture/examples/test_mixtureGP.py
+++ GPMixture/examples/test_mixtureGP.py
@@ -4,17 +4,21 @@
 from test_common import *
 from gp_code.mixture_gp import mixtureOfGPs
 
+
 # Read the areas file, dataset, and form the goalsLearnedStructure object
-goalsData, pathMat, __ = read_and_filter('parameters/CentralStation_GoalsDescriptions.csv','datasets/GCS/CentralStation_trainingSet.txt')
-stepUnit   = 0.0438780780171   #get_number_of_steps_unit(pathMat, nGoals)
+goalsDescriptions= 'parameters/CentralStation_GoalsDescriptions.csv'
+trajFile         = 'datasets/GC/Annotation/'
+imgGCS           = 'imgs/train_station.jpg'
+
+traj_dataset, goalsData, trajMat, __ = read_and_filter('GCS',goalsDescriptions,trajFile,use_pickled_data=True)
 
 # Selection of the kernel type
-kernelType = "linePriorCombined"#"combined"
-nParameters= 4
+kernelType = "linePriorCombined"
+nParameters = 4
 
 # Read the kernel parameters from file
-goalsData.kernelsX = read_and_set_parameters("parameters/linearpriorcombined20x20_x.txt",nParameters)
-goalsData.kernelsY = read_and_set_parameters("parameters/linearpriorcombined20x20_y.txt",nParameters)
+goalsData.kernelsX = read_and_set_parameters('parameters/linearpriorcombined20x20_x.txt',nParameters)
+goalsData.kernelsY = read_and_set_parameters('parameters/linearpriorcombined20x20_y.txt',nParameters)
 
 """******************************************************************************"""
 """**************    Testing                           **************************"""
@@ -28,14 +32,15 @@ kernelX = goalsData.kernelsX[startG][nextG]
 kernelY = goalsData.kernelsY[startG][nextG]
 
 # Get the ground truth path
-_path = pathMat[startG][nextG][pathId]
+_path = trajMat[startG][nextG][pathId]
 # Get the path data
-pathX, pathY, pathL, pathT = _path.x, _path.y, _path.l, _path.t
+pathX, pathY, pathT = _path
+pathL = trajectory_arclength(_path)
 # Total path length
 pathSize = len(pathX)
 
 # Prediction of single paths with a mixture model
-mgps     = mixtureOfGPs(startG,stepUnit,goalsData)
+mgps     = mixtureOfGPs(startG,goalsData)
 nSamples = 50
 
 # Divides the trajectory in part_num parts and consider
