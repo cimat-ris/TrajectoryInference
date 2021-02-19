4c9e5fcd4dd35b2a77ff40e9d132976c48a03e4f
Ana Karen Lopez Carbajal <67447283+aklopezcarbajal@users.noreply.github.com>
2021-02-19 18:36:49 -0500

    Update mixture_gpT.py

--- GPMixture/gp_code/mixture_gpT.py
+++ GPMixture/gp_code/mixture_gpT.py
@@ -37,11 +37,27 @@ class mixtureGPT:
         self.sqRootVarY      = np.empty(n, dtype=object)
         self.observedX       = None
         self.observedY       = None
-        self.observedL       = None
+        self.observedT       = None
         # The basic element here is this object, that will do the regression work
         self.gpPathRegressor = [None]*n
         self.gpTrajectoryRegressor = [None]*n
         for i in range(self.goalsData.nGoals):
             # One regressor per goal
-            self.gpPathRegressor[i] = path_regression(self.goalsData.kernelsX[self.startG][i], self.goalsData.kernelsY[self.startG][i],goalsData.distUnit,self.stepUnit,self.goalsData.areas_coordinates[i],self.goalsData.areas_axis[i])
-            #self.gpTrajectoryRegressor[i] = trajectory_regression(self.goalsData.kernelsX[self.startG][i], self.goalsData.kernelsY[self.startG][i],goalsData.distUnit,stepUnit,self.goalsData.areas_coordinates[i],self.goalsData.areas_axis[i])
+            timeTransitionData = [self.goalsData.timeTransitionMeans[self.startG][i],self.goalsData.timeTransitionStd[self.startG][i]]
+            self.gpPathRegressor[i] = path_regression(self.goalsData.kernelsX[self.startG][i], self.goalsData.kernelsY[self.startG][i],None,None,self.goalsData.areas_coordinates[i],self.goalsData.areas_axis[i],None,'Trautman',timeTransitionData)
+    
+    def update(self, observations):
+        self.observedX = observations[:,0]
+        self.observedY = observations[:,1]
+        self.observedT = observations[:,2]
+        # Update each regressor with its corresponding observations
+        for i in range(self.goalsData.nGoals):
+            goalCenter,__= goal_center_and_size(self.goalsData.areas_coordinates[i])
+            distToGoal   = euclidean_distance([self.observedX[-1],self.observedY[-1]], goalCenter)
+            dist         = euclidean_distance([self.observedX[0],self.observedY[0]], goalCenter)
+            # Update observations and re-compute the kernel matrices
+            self.gpPathRegressor[i].update_observations(observations)
+            # Compute the model likelihood
+            self.goalsLikelihood[i] = self.gpPathRegressor[i].compute_likelihood(observations,self.nPoints)
+
+
