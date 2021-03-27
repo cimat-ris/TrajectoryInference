22feddc91604610f391667e50c6fd27b50edb877
Ana Karen Lopez Carbajal <67447283+aklopezcarbajal@users.noreply.github.com>
2021-03-26 20:38:40 -0400

    Working on interaction test

--- GPMixture/examples/test_interaction.py
+++ GPMixture/examples/test_interaction.py
@@ -7,7 +7,7 @@ import random
 from test_common import *
 from gp_code.kernels import *
 from gp_code.mixture_gpT import mixtureGPT
-from utils.manip_trajectories import start_time
+from utils.manip_trajectories import start_time, get_trajectories_given_time_interval, get_goal_of_point
 
 # Read the areas file, dataset, and form the goalsLearnedStructure object
 goalsDescriptions= '../parameters/CentralStation_GoalsDescriptions.csv'
@@ -23,3 +23,52 @@ goalsData.kernelsY = create_kernel_matrix('combinedTrautman', goalsData.nGoals, 
 
 # Sort by start time
 filtered.sort(key=start_time)
+# Find trajectories within an interval
+trajs = get_trajectories_given_time_interval(filtered, 0, 100)
+print('Number of trajs: ', len(trajs))
+
+startGoals = []
+for traj in trajs:
+    p = [traj[0][0], traj[1][0] ]
+    startGoals.append( get_goal_of_point(p, goalsData.areas_coordinates) )
+    
+print('Goals of trajectories:')
+print( np.unique(startGoals) )
+goalIndex = np.unique(startGoals)
+
+mgps = [mixtureGPT(i,goalsData) for i in goalIndex ]
+observations = np.empty(goalIndex.size )
+nSamples = 5
+
+part_num = 3
+observedTime = 100
+
+# For different sub-parts of the trajectory
+for i in range(1,part_num-1):
+    p = plotter("../imgs/train_station.jpg")
+    p.plot_scene_structure(goalsData)
+
+    knownN = int((i+1)*(observedTime/part_num)) #numero de datos conocidos
+    
+    #get observations of each trajectory
+    #compute samples
+    #Evaluate likelihood of samples
+   
+    #observations = observed_data(path,knownN)
+    """Multigoal prediction test"""
+    #print('[INF] Updating likelihoods')
+    #likelihoods = mgps.update(observations)
+
+    #print('[INF] Performing prediction')
+    #predictedXYVec,varXYVec = mgps.predict_path()
+    #print('[INF] Plotting')
+    #p.plot_multiple_predictions_and_goal_likelihood(path[0],path[1],knownN,goalsData.nGoals,likelihoods,predictedXYVec,varXYVec)
+
+    #print("[RES] Goals likelihood\n",mgps.goalsLikelihood)
+    #print("[RES] Mean likelihood:", mgps.meanLikelihood)
+    #print('[INF] Generating samples')
+    #samplePaths = mgps.sample_paths(nSamples)
+    #p.plot_path_samples_with_observations(observations,samplePaths)
+    #p.show() 
+
+
