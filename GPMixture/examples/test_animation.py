eb0a910916d9c267e5184061e728c2456bb24bfa
Jean Bernard Hayet <jbhayet@cimat.mx>
2021-03-29 17:48:53 -0600

    Repaired the animation example

--- GPMixture/examples/test_animation.py
+++ GPMixture/examples/test_animation.py
@@ -3,6 +3,7 @@
 """
 from test_common import *
 from gp_code.mixture_gp import mixtureOfGPs
+from gp_code.single_gp import singleGP
 
 # Read the areas file, dataset, and form the goalsLearnedStructure object
 goalsDescriptions= './parameters/CentralStation_GoalsDescriptions.csv'
@@ -22,14 +23,14 @@ goalsData.kernelsY = read_and_set_parameters("parameters/linearpriorcombined20x2
 
 """**********          Testing          ***********"""
 # We give the start and ending goals
-startG, nextG, pathId = 0,6,7
-
+startG, endG = 0,6
+pathId       = np.random.randint(0,len(trajMat[startG][endG]))
 # Kernels for this pair of goals
-kernelX = goalsData.kernelsX[startG][nextG]
-kernelY = goalsData.kernelsY[startG][nextG]
+kernelX = goalsData.kernelsX[startG][endG]
+kernelY = goalsData.kernelsY[startG][endG]
 
 # Get the ground truth path
-path = trajMat[startG][nextG][pathId]
+path = trajMat[startG][endG][pathId]
 # Get the path data
 pathX, pathY, pathT = path
 pathL = trajectory_arclength(path)
@@ -37,19 +38,33 @@ pathL = trajectory_arclength(path)
 pathSize = len(pathX)
 
 # Test function: prediction of single paths with multiple goals
-mgps = mixtureOfGPs(startG,goalsData)
+gp = singleGP(startG,endG,goalsData)
 
-part_num = 10
+p = plotter()
 # For different sub-parts of the trajectory
-for i in range(1,part_num-1):
-    knownN = int((i+1)*(pathSize/part_num)) 
-    trueX,trueY,trueL = observed_data(pathX,pathY,pathL,knownN)
-    """Multigoal prediction test"""
+for knownN in range(10,pathSize):
+    p.set_background(imgGCS)
+    p.plot_scene_structure(goalsData)
+    observations, ground_truth = observed_data([pathX,pathY,pathL,pathT],knownN)
+    """Single goal prediction test"""
     print('[INF] Updating likelihoods')
-    likelihoods = mgps.update(trueX,trueY,trueL)
-    print('[INF] Performing prediction')
-    predictedXYVec,varXYVec = mgps.predict()
+    # Update the GP with (real) observations
+    start        = time.process_time()
+    likelihood   = gp.update(observations)
+    stop         = time.process_time()
+    filteredPath = gp.filter()
+    print("[INF] CPU process time (update): %.1f [ms]" % (1000.0*(stop-start)))
+    start = stop
+    # Perform prediction
+    predictedXY,varXY = gp.predict_trajectory()
+    stop       = time.process_time()
+    print("[INF] CPU process time (prediction): %.1f [ms]" % (1000.0*(stop-start)))
+    print("[RES] Likelihood: ",likelihood)
     print('[INF] Plotting')
-    animate_multiple_predictions_and_goal_likelihood(img,pathX,pathY,knownN,goalsData.nGoals,likelihoods,predictedXYVec,varXYVec,False)
-    if i==3:
-        animate_multiple_predictions_and_goal_likelihood(img,pathX,pathY,knownN,goalsData.nGoals,likelihoods,predictedXYVec,varXYVec,True)
+    # Plot the filtered version of the observations
+    p.plot_filtered(filteredPath)
+    # Plot the prediction
+    p.plot_prediction(observations,predictedXY,varXY)
+    # Plot the ground truth
+    p.plot_ground_truth(ground_truth)
+    p.pause(0.05)
