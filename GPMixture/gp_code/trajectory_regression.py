4c19d300fb85065b06c03c1344ddba6e2fa82e07
JBH <jbhayet@cimat.mx>
2021-04-08 20:07:49 -0500

    Prediction on future timesteps

--- GPMixture/gp_code/trajectory_regression.py
+++ GPMixture/gp_code/trajectory_regression.py
@@ -14,22 +14,25 @@ class trajectory_regression(path_regression):
         # Init of the base class
         super(trajectory_regression, self).__init__(kernelX, kernelY, sigmaNoise, unit, stepUnit, finalArea, prior)
         # Regression model for the relative value of speed
-        self.speedModel  = speedModel
-        self.speedAverage= 1.0
+        self.speedModel       = speedModel
+        self.speedAverage     = 1.0
+        self.currentTime      = 0.0
+        self.currentArcLength = 0.0
 
     # Prediction of the trajectory
     def predict_trajectory_to_finish_point(self,compute_sqRoot=False):
         # Predict path
         path, var   = self.predict_path_to_finish_point(compute_sqRoot)
-        arc_lengths = path[:,2]
+        arc_lengths     = path[:,2]
+        arc_lengths_ext = np.concatenate([[self.currentArcLength],path[:,2]])
         # Use the speed model to predict relative speed with respect to the average
         if isinstance(self.speedModel, int):
             predicted_relative_speeds = np.ones(arc_lengths.shape)
         else:
             predicted_relative_speeds = self.speedModel.predict(arc_lengths.reshape(-1, 1))
         predicted_speeds          = self.speedAverage*predicted_relative_speeds
-        times = np.divide(arc_lengths[1:]-arc_lengths[:-1],predicted_speeds[:-1])
-        trajectory = np.concatenate([path,predicted_speeds.reshape(-1, 1)],axis=1)
+        times = self.currentTime + np.divide(arc_lengths_ext[1:]-arc_lengths_ext[:-1],predicted_speeds).reshape(-1,1)
+        trajectory = np.concatenate([path,times,predicted_speeds.reshape(-1, 1)],axis=1)
         return trajectory, var
 
     # Update observations
@@ -41,7 +44,11 @@ class trajectory_regression(path_regression):
         weights = np.linspace(0.0,1.0,num=nobs)
         weights = weights/np.sum(weights)
         self.speedAverage = np.average(observations[:,4],axis=0,weights=weights)
-        print('[INF] Speed average ',self.speedAverage)
+        print('[INF] Speed average: ',self.speedAverage)
+        self.currentTime = observations[-1,3]
+        print('[INF] Current time: ',self.currentTime)
+        self.currentArcLength = observations[-1,2]
+        print('[INF] Current arc-length: ',self.currentArcLength)
 
     # Generate a sample from perturbations
     def sample_trajectory_with_perturbation(self,deltaX,deltaY):
