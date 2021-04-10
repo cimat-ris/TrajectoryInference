3bc7035a11dd9825569019025e81168dc8850c1e
JBH <jbhayet@cimat.mx>
2021-04-09 19:07:01 -0500

    Trajectories animations!

--- GPMixture/examples/test_animation_single_trajectory.py
+++ GPMixture/examples/test_animation_single_trajectory.py
@@ -4,6 +4,7 @@
 from test_common import *
 from gp_code.single_gp import singleGP
 import matplotlib.pyplot as plt
+from matplotlib import animation
 
 # Read the areas file, dataset, and form the goalsLearnedStructure object
 goalsDescriptions= './parameters/CentralStation_GoalsDescriptions.csv'
@@ -50,18 +51,21 @@ pathSize = len(pathX)
 # Test function: prediction of single trajectories with a single goal
 gp = singleGP(startG,endG,goalsData)
 
-fig, ax = plt.subplots(4,1)
-ax[0].set_ylabel('x')
-ax[0].set_xlabel('l')
-ax[1].set_ylabel('y')
-ax[1].set_xlabel('l')
-ax[2].set_ylabel('v')
-ax[2].set_xlabel('l')
-ax[3].set_ylabel('t')
-ax[3].set_xlabel('l')
-
 # For different sub-parts of the trajectory
-for knownN in range(10,pathSize-1):
+for knownN in range(10,pathSize-1,10):
+    fig, ax = plt.subplots(4,1)
+    ax[0].set_ylabel('x')
+    ax[0].set_xlabel('l')
+    ax[1].set_ylabel('y')
+    ax[1].set_xlabel('l')
+    ax[2].set_ylabel('v')
+    ax[2].set_xlabel('l')
+    ax[3].set_ylabel('t')
+    ax[3].set_xlabel('l')
+    p = plotter()
+    p.set_background(imgGCS)
+    p.plot_scene_structure(goalsData)
+
     print('--------------------------')
     # Observations: x,y,l,t
     # Ground truth: x,y,t
@@ -86,8 +90,35 @@ for knownN in range(10,pathSize-1):
     ax[3].axis([0,np.max(pathL),np.min(pathT),np.max(pathT)])
     ax[3].plot(predictedXYLTV[:,2],predictedXYLTV[:,3],'b')
     ax[3].plot(observations[:,2],observations[:,3],'r')
-    plt.pause(0.15)
-    ax[0].cla()
-    ax[1].cla()
-    ax[2].cla()
-    ax[3].cla()
+    times_predicted = predictedXYLTV[:,3]
+    x_predicted     = predictedXYLTV[:,0]
+    y_predicted     = predictedXYLTV[:,1]
+    times_animated  = np.arange(np.min(times_predicted),np.max(times_predicted),0.1)
+    x_interpolated  = np.interp(times_animated,times_predicted,x_predicted)
+    y_interpolated  = np.interp(times_animated,times_predicted,y_predicted)
+    # Plot the filtered version of the observations
+    p.plot_filtered(filteredPath)
+    # Plot the prediction
+    p.plot_prediction(observations,predictedXYLTV,varXY)
+    # Plot the ground truth
+    p.plot_ground_truth(ground_truth)
+    # Patch for the pedestrian
+    patch = plt.Circle((0,0),25, fc='b')
+    def init():
+        patch.center = (observations[-1,0],observations[-1,1])
+        p.ax.add_patch(patch)
+        return patch,
+
+    def animate(i):
+        x, y = patch.center
+        x = x_interpolated[i]
+        y = y_interpolated[i]
+        patch.center = (x, y)
+        return patch,
+
+    anim = animation.FuncAnimation(fig, animate,
+                               init_func=init,
+                               frames=x_interpolated.shape[0],
+                               interval=100,
+                               blit=True)
+    plt.show()
