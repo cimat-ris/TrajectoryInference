6e8f2d7bd7ea371f8cea91121feaa8788e66f05b
Jean Bernard Hayet <jbhayet@cimat.mx>
2021-06-03 20:15:19 -0500

    Updated another xample

--- examples/test_animation_single.py
+++ examples/test_animation_single.py
@@ -5,70 +5,91 @@ from test_common import *
 from gp_code.mixture_gp import mixtureOfGPs
 from gp_code.single_gp import singleGP
 
-# Read the areas file, dataset, and form the goalsLearnedStructure object
-img_bckgd        = './datasets/GC/reference.jpg'
-img_bckgd        = './datasets/Edinburgh/edinburgh.jpg'
-coordinates      = "img"
 
-traj_dataset, goalsData, trajMat, __ = read_and_filter('EIF',coordinate_system=coordinates,use_pickled_data=True)
 
-# Selection of the kernel type
-kernelType  = "linePriorCombined"
-nParameters = 4
 
-# Read the kernel parameters from file
-goalsData.kernelsX = read_and_set_parameters("parameters/linearpriorcombined20x20_EIF_img_x.txt",nParameters)
-goalsData.kernelsY = read_and_set_parameters("parameters/linearpriorcombined20x20_EIF_img_y.txt",nParameters)
+def main():
+    # Parsing arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--coordinates', default='img',help='Type of coordinates (img or world)')
+    parser.add_argument('--dataset_id', '--id',default='GCS',help='dataset id, GCS or EIF (default: GCS)')
+    parser.add_argument('--log_level',type=int, default=20,help='Log level (default: 20)')
+    parser.add_argument('--log_file',default='',help='Log file (default: standard output)')
+    args = parser.parse_args()
+    if args.log_file=='':
+        logging.basicConfig(format='%(levelname)s: %(message)s',level=args.log_level)
+    else:
+        logging.basicConfig(filename=args.log_file,format='%(levelname)s: %(message)s',level=args.log_level)
 
-"""**********          Testing          ***********"""
-# We give the start and ending goals
-randomPath = True
-if randomPath:
-    flag = True
-    while flag:
-        startG, endG = random.randrange(goalsData.goals_n), random.randrange(goalsData.goals_n)
-        if len(trajMat[startG][endG])>0 and goalsData.kernelsX[startG][endG].optimized is True:
-            pathId = random.randrange( len(trajMat[startG][endG]) )
-            flag = False
-    print("[INF] Selected goals:",(startG,endG),"| path index:", pathId)
-else:
-    startG,endG = 0, 7
-    pathId       = np.random.randint(0,len(trajMat[startG][endG]))
-# Kernels for this pair of goals
-kernelX = goalsData.kernelsX[startG][endG]
-kernelY = goalsData.kernelsY[startG][endG]
-# Get the ground truth path
-path = trajMat[startG][endG][pathId]
-# Get the path data
-pathX, pathY, pathT = path
-pathL = trajectory_arclength(path)
+    # Read the areas file, dataset, and form the goalsLearnedStructure object
+    coordinates      = args.coordinates
+    # Read the areas file, dataset, and form the goalsLearnedStructure object
+    img_bckgd        = './datasets/GC/reference.jpg'
+    #img_bckgd        = './datasets/Edinburgh/edinburgh.jpg'
 
-# Total path length
-pathSize = len(pathX)
+    traj_dataset, goalsData, trajMat, __ = read_and_filter(args.dataset_id,coordinate_system=coordinates,use_pickled_data=True)
 
-# Test function: prediction of single paths with multiple goals
-gp = singleGP(startG,endG,goalsData)
+    # Selection of the kernel type
+    kernelType  = "linePriorCombined"
+    nParameters = 4
 
-p = plotter()
-# For different sub-parts of the trajectory
-for knownN in range(10,pathSize-1):
-    print('--------------------------')
-    p.set_background(img_bckgd)
-    p.plot_scene_structure(goalsData)
-    observations, ground_truth = observed_data([pathX,pathY,pathL,pathT],knownN)
-    """Single goal prediction test"""
-    print('[INF] Updating observations')
-    # Update the GP with (real) observations
-    likelihood   = gp.update(observations)
-    filteredPath = gp.filter()
-    # Perform prediction
-    predictedXY,varXY = gp.predict_trajectory()
-    print("[RES] Likelihood: ",likelihood)
-    print('[INF] Plotting')
-    # Plot the filtered version of the observations
-    p.plot_filtered(filteredPath)
-    # Plot the prediction
-    p.plot_prediction(observations,predictedXY,varXY)
-    # Plot the ground truth
-    p.plot_ground_truth(ground_truth)
-    p.pause(0.05)
+    # Read the kernel parameters from file
+    goalsData.kernelsX = read_and_set_parameters("parameters/linearpriorcombined20x20_EIF_img_x.txt",nParameters)
+    goalsData.kernelsY = read_and_set_parameters("parameters/linearpriorcombined20x20_EIF_img_y.txt",nParameters)
+
+    """**********          Testing          ***********"""
+    # We give the start and ending goals
+    randomPath = True
+    if randomPath:
+        flag = True
+        while flag:
+            startG, endG = random.randrange(goalsData.goals_n), random.randrange(goalsData.goals_n)
+            if len(trajMat[startG][endG])>0 and goalsData.kernelsX[startG][endG].optimized is True:
+                pathId = random.randrange( len(trajMat[startG][endG]) )
+                flag = False
+        logging.info('Selected goals: {:d} {:d}| path index:{:d}'.format(startG,endG,pathId))
+    else:
+        startG,endG = 0, 7
+        pathId       = np.random.randint(0,len(trajMat[startG][endG]))
+    # Kernels for this pair of goals
+    kernelX = goalsData.kernelsX[startG][endG]
+    kernelY = goalsData.kernelsY[startG][endG]
+    # Get the ground truth path
+    path = trajMat[startG][endG][pathId]
+    # Get the path data
+    pathX, pathY, pathT = path
+    pathL = trajectory_arclength(path)
+
+    # Total path length
+    pathSize = len(pathX)
+
+    # Test function: prediction of single paths with multiple goals
+    gp = singleGP(startG,endG,goalsData)
+
+    p = plotter()
+    # For different sub-parts of the trajectory
+    for knownN in range(10,pathSize-1):
+        logging.info('--------------------------')
+        p.set_background(img_bckgd)
+        p.plot_scene_structure(goalsData)
+        observations, ground_truth = observed_data([pathX,pathY,pathL,pathT],knownN)
+        """Single goal prediction test"""
+        logging.info('Updating observations')
+        # Update the GP with (real) observations
+        likelihood   = gp.update(observations)
+        filteredPath = gp.filter()
+        # Perform prediction
+        predictedXY,varXY = gp.predict_trajectory()
+        logging.info('Likelihood: {}'.format(likelihood))
+        logging.info('Plotting')
+        # Plot the filtered version of the observations
+        p.plot_filtered(filteredPath)
+        # Plot the prediction
+        p.plot_prediction(observations,predictedXY,varXY)
+        # Plot the ground truth
+        p.plot_ground_truth(ground_truth)
+        p.pause(0.05)
+
+
+if __name__ == '__main__':
+    main()
