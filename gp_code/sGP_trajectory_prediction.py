be8ce14735f039af0b1a36ab58c78a82273cdc1d
Jean Bernard Hayet <jbhayet@cimat.mx>
2021-06-19 19:07:38 -0500

    Renamed class

--- gp_code/sGP_trajectory_prediction.py
+++ gp_code/sGP_trajectory_prediction.py
@@ -6,7 +6,7 @@ import math
 from gp_code.trajectory_regression import *
 
 # Class for performing path regression with a single Gaussian Process
-class singleGP:
+class sGP_trajectory_prediction:
 
     # Constructor
     def __init__(self, startG, endG, goalsData, mode = None):
@@ -16,6 +16,10 @@ class singleGP:
         self.endG            = endG
         self.predictedMeans  = None
         self.predictedVars   = None
+        self._current_time      = 0.0
+        self._current_arc_length= 0.0
+        self._speed_average     = 1.0
+
         # The basic element here is this object, that will do the regression work
         if mode == 'Trautman': #!Using path_regression for now
             timeTransitionData = [self.goalsData.timeTransitionMeans[self.startG][self.endG],self.goalsData.timeTransitionStd[self.startG][self.endG]]
@@ -25,6 +29,15 @@ class singleGP:
 
     # Update observations and compute likelihood based on observations
     def update(self,observations):
+        # Update time and current arc length
+        self._current_time      = observations[-1,3]
+        self._current_arc_length= observations[-1,2]
+        logging.info('Time: {:2.2f} Arc-length: {:4.2f} Speed: {:2.2f}'.format(self._current_time,self._current_arc_length,self._speed_average))
+        # Average speed
+        nobs    = observations.shape[0]
+        weights = np.linspace(0.0,1.0,num=nobs)
+        weights = weights/np.sum(weights)
+        self._speed_average    = np.average(observations[:,4],axis=0,weights=weights)
         # Update observations and re-compute the kernel matrices
         self.gpTrajectoryRegressor.update_observations(observations)
         # Compute the model likelihood
@@ -39,7 +52,7 @@ class singleGP:
     # Performs trajectory prediction
     def predict_trajectory(self,compute_sqRoot=False):
         # Uses the already computed matrices to apply regression over missing data
-        self.predictedMeans, self.predictedVars = self.gpTrajectoryRegressor.predict_trajectory_to_finish_point(compute_sqRoot=compute_sqRoot)
+        self.predictedMeans, self.predictedVars = self.gpTrajectoryRegressor.predict_trajectory_to_finish_point(self._speed_average,self._current_time,self._current_arc_length,compute_sqRoot=compute_sqRoot)
         return self.predictedMeans,self.predictedVars
 
     # Get a filtered version of the initial observations
