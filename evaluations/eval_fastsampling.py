a2381148cd12ce69661773aac7e77e9de9ec4e7b
JB Hayet <jbhayet@cimat.mx>
2021-07-20 15:08:26 -0500

    Rewrote the fastsampling test to produce comparisons for the efficient sampling scheme

--- evaluations/eval_fastsampling.py
+++ evaluations/eval_fastsampling.py
@@ -4,14 +4,15 @@
 import sys, os, argparse, logging,random
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from utils.io_parameters import read_and_set_parameters
-from utils.io_trajectories import read_and_filter
 from utils.manip_trajectories import observed_data
 from utils.stats_trajectories import trajectory_arclength, trajectory_speeds
 from gp_code.mGP_trajectory_prediction import mGP_trajectory_prediction
 from utils.plotting import plotter, plot_path_samples
 from utils.plotting import animate_multiple_predictions_and_goal_likelihood
+from utils.io_trajectories import read_and_filter,partition_train_test
 import numpy as np
-import time
+import time,pickle
+from tqdm import tqdm
 
 def main():
     # Parsing arguments
@@ -20,8 +21,8 @@ def main():
     parser.add_argument('--dataset_id', '--id',default='GCS',help='dataset id, GCS or EIF (default: GCS)')
     parser.add_argument('--log_level',type=int, default=20,help='Log level (default: 20)')
     parser.add_argument('--log_file',default='',help='Log file (default: standard output)')
-    parser.add_argument('--no_pickle', dest='pickle',action='store_false')
-    parser.set_defaults(pickle=True)
+    parser.add_argument('--pickle', dest='pickle', action='store_true',help='uses previously pickled data')
+    parser.set_defaults(pickle=False)
     args = parser.parse_args()
     if args.log_file=='':
         logging.basicConfig(format='%(levelname)s: %(message)s',level=args.log_level)
@@ -31,63 +32,79 @@ def main():
     # Read the areas file, dataset, and form the goalsLearnedStructure object
     imgGCS           = './datasets/GC/reference.jpg'
     coordinates      = args.coordinates
-    traj_dataset, goalsData, trajMat, __ = read_and_filter('GCS',coordinate_system=coordinates,use_pickled_data=args.pickle)
+    dataset_name     = args.dataset_id
+    traj_dataset, goalsData, trajMat, __ = read_and_filter(dataset_name,coordinate_system=coordinates,use_pickled_data=args.pickle)
 
     # Selection of the kernel type
     kernelType  = "linePriorCombined"
-    nParameters = 4
 
-    # Read the kernel parameters from file
-    goalsData.kernelsX = read_and_set_parameters("parameters/linearpriorcombined20x20_GCS_img_x.txt",nParameters)
-    goalsData.kernelsY = read_and_set_parameters("parameters/linearpriorcombined20x20_GCS_img_y.txt",nParameters)
+    # Partition test/train set
+    pickle_dir='pickle'
+    if args.pickle==False:
+        train_trajectories_matrix, test_trajectories_matrix = partition_train_test(trajMat,training_ratio=0.2)
+        pickle_out = open(pickle_dir+'/train_trajectories_matrix-'+args.dataset_id+'-'+coordinates+'.pickle',"wb")
+        pickle.dump(train_trajectories_matrix, pickle_out, protocol=2)
+        pickle_out.close()
+        pickle_out = open(pickle_dir+'/test_trajectories_matrix-'+args.dataset_id+'-'+coordinates+'.pickle',"wb")
+        pickle.dump(train_trajectories_matrix, pickle_out, protocol=2)
+        pickle_out.close()
+
+        """**************    Learning GP parameters     **************************"""
+        logging.info("Starting the learning phase")
+        goalsData.optimize_kernel_parameters(kernelType,train_trajectories_matrix)
+        pickle_out = open(pickle_dir+'/goalsData-'+args.dataset_id+'-'+coordinates+'.pickle',"wb")
+        pickle.dump(goalsData, pickle_out, protocol=2)
+        pickle_out.close()
+    else:
+        pickle_in = open(pickle_dir+'/train_trajectories_matrix-'+args.dataset_id+'-'+coordinates+'.pickle', "rb")
+        train_trajectories_matrix = pickle.load(pickle_in)
+        pickle_in = open(pickle_dir+'/test_trajectories_matrix-'+args.dataset_id+'-'+coordinates+'.pickle', "rb")
+        test_trajectories_matrix = pickle.load(pickle_in)
+        pickle_in = open(pickle_dir+'/goalsData-'+args.dataset_id+'-'+coordinates+'.pickle',"rb")
+        goalsData = pickle.load(pickle_in)
 
     """**********          Testing          ***********"""
-    # We give the start and ending goals
-    startG,endG = 0, 7
-    pathId       = np.random.randint(0,len(trajMat[startG][endG]))
-    # Kernels for this pair of goals
-    kernelX = goalsData.kernelsX[startG][endG]
-    kernelY = goalsData.kernelsY[startG][endG]
-    # Get the ground truth path
-    path = trajMat[startG][endG][pathId]
-    # Get the path data
-    pathX, pathY, pathT = path
-    pathL = trajectory_arclength(path)
+    timesEff  = []
+    timesSlow = []
+    ratios    = []
+    for startG, endG in tqdm(np.ndindex(goalsData.goals_n,goalsData.goals_n),total=goalsData.goals_n*goalsData.goals_n):
+            if goalsData.kernelsX[startG][endG] is not None and goalsData.kernelsY[startG][endG] is not None:
+                for trajectory in test_trajectories_matrix[startG][endG]:
+                    # Get the path data
+                    pathX, pathY, pathT = trajectory
+                    pathL = trajectory_arclength(trajectory)
 
-    # Total path length
-    pathSize = len(pathX)
-    nSamples = 1
-    # Prediction of single paths with a mixture model
-    mgps     = mGP_trajectory_prediction(startG,goalsData)
+                    # Total path length
+                    pathSize = len(pathX)
+                    nSamples = 100
+                    # Prediction of single paths with a mixture model
+                    mgps     = mGP_trajectory_prediction(startG,goalsData)
 
-    # For different sub-parts of the trajectory
-    for knownN in range(5,pathSize-1):
-        p = plotter()
-        p.set_background(imgGCS)
-        p.plot_scene_structure(goalsData,draw_ids=True)
-        logging.info("--------------------------")
-        tic = time.perf_counter()
-        # Monte Carlo
-        observations, ground_truth = observed_data([pathX,pathY,pathL,pathT],knownN)
-        """Multigoal prediction test"""
-        logging.debug('Updating likelihoods')
-        likelihoods  = mgps.update(observations)
-        filteredPaths= mgps.filter()
-        logging.debug('Performing prediction')
-        predictedXYVec,varXYVec = mgps.predict_trajectory(compute_sqRoot=True)
-        logging.debug('Generating samples')
-        paths = mgps.sample_paths(nSamples)
-        # Perform prediction
-        toc = time.perf_counter()
-        logging.info('Time in s {}'.format(toc-tic))
-        logging.debug("Plotting")
-        p.plot_path_samples_with_observations(observations,[paths[0][0]],spec_color='red')
-        p.plot_path_samples_with_observations(observations,[paths[0][1]],spec_color='green')
-        logging.info("Goals likelihood {}".format(likelihoods))
-        logging.info("Mean likelihood: {}".format(mgps.meanLikelihood))
-        # Plot the ground truth
-        p.plot_ground_truth(ground_truth)
-        p.show()
+                    # Take half of the trajectory
+                    knownN = pathSize//4
+                    logging.debug("--------------------------")
+                    # Monte Carlo
+                    observations, ground_truth = observed_data([pathX,pathY,pathL,pathT],knownN)
+                    """Multigoal prediction test"""
+                    likelihoods  = mgps.update(observations)
+                    if likelihoods is None:
+                        continue
+                    filteredPaths= mgps.filter()
+                    predictedXYVec,varXYVec = mgps.predict_trajectory(compute_sqRoot=True)
+                    logging.debug('Generating samples')
+                    tic = time.perf_counter()
+                    paths = mgps.sample_paths(nSamples,efficient=True)
+                    toc = time.perf_counter()
+                    timesEff.append(toc-tic)
+                    tic = time.perf_counter()
+                    paths = mgps.sample_paths(nSamples,efficient=False)
+                    toc = time.perf_counter()
+                    timesSlow.append(toc-tic)
+                    ratios.append(timesEff[-1]/timesSlow[-1])
+                    #        print("Largest deviations in x: {} and y: {}".format(np.max(np.abs(predictedX2-predictedX)),np.max(np.abs(predictedY2-predictedY))))
+    print(np.mean(timesEff))
+    print(np.mean(timesSlow))
+    print(np.median(ratios))
 
 if __name__ == '__main__':
     main()
