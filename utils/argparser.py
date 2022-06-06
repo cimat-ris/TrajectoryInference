70b8df259ee4a9b18d80f7dfa742a84a25d63b14
Jean Bernard Hayet <jbhayet@cimat.mx>
2022-06-06 12:33:00 -0500

    Moved the parsing to another file to avoid redundant code

---
+++ utils/argparser.py
@@ -0,0 +1,21 @@
+import argparse
+import logging
+
+def load_args_logs():
+    # Parsing arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--coordinates', default='img',help='Type of coordinates (img or world)')
+    parser.add_argument('--dataset_id', '--id',default='GCS',help='dataset id, GCS or EIF (default: GCS)')
+    parser.add_argument('--log_level',type=int, default=20,help='Log level (default: 20)')
+    parser.add_argument('--log_file',default='',help='Log file (default: standard output)')
+    parser.add_argument('--pickle', dest='pickle', action='store_true',help='uses previously pickled data')
+    parser.set_defaults(pickle=False)
+    parser.add_argument('--no_draw_trajs', dest='no_draw_trajs', action='store_true',help='Draws trajectories samples')
+    parser.set_defaults(no_draw_trajs=False)
+    args = parser.parse_args()
+    # Set up logging options
+    if args.log_file=='':
+        logging.basicConfig(format='%(levelname)s: %(message)s',level=args.log_level)
+    else:
+        logging.basicConfig(filename=args.log_file,format='%(levelname)s: %(message)s',level=args.log_level)
+    return args
