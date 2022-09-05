c02c07215bc1f3cf2b96d0fb7cdb3dc6b1517b7d
Jean Bernard Hayet <jbhayet@cimat.mx>
2022-09-04 22:10:27 -0500

    Misc

--- examples/test_common.py
+++ examples/test_common.py
@@ -5,15 +5,16 @@ import logging
 import argparse
 import pandas as pd
 import numpy as np
-import time
+import time,pickle
 import matplotlib.image as mpimg
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import gp_code
+from gp_code.likelihood import ADE,FDE
 from gp_code.goal_pairs import goal_pairs
 from utils.io_parameters import read_and_set_parameters
-from utils.io_trajectories import read_and_filter,get_traj_from_file
+from utils.io_trajectories import read_and_filter,get_traj_from_file,partition_train_test
 from utils.manip_trajectories import observed_data,get_trajectories_given_time_interval,separate_trajectories_between_goals
 from utils.plotting import plotter, multiple_plotter, plot_path_samples
 from utils.plotting import animate_multiple_predictions_and_goal_likelihood
